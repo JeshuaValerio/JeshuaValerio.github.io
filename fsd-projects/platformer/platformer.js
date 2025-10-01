dc6d69090ec96a8085c2734ae48ea61f8880d87a
JeshuaValerio <jeshua.valerio@jpschools.org>
2025-10-01 15:56:31 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,58 +1,74 @@
 const { createContext } = require("react");
 
 $(function () {
-  // initialize canvas and context when able to
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  window.addEventListener("load", loadJson);
-
-  function setup() {
-    if (firstTimeSetup) {
-      halleImage = document.getElementById("player");
-      projectileImage = document.getElementById("projectile");
-      cannonImage = document.getElementById("cannon");
-      $(document).on("keydown", handleKeyDown);
-      $(document).on("keyup", handleKeyUp);
-      firstTimeSetup = false;
-      //start game
-      setInterval(main, 1000 / frameRate);
-    }
-
-    // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
-
-    //////////////////////////////////
-    // ONLY CHANGE BELOW THIS POINT //
-    //////////////////////////////////
-
-    // TODO 1 - Enable the Grid
-   toggleGrid();
+ // initialize canvas and context when able to
+ canvas = document.getElementById("canvas");
+ ctx = canvas.getContext("2d");
+ window.addEventListener("load", loadJson);
 
 
-    // TODO 2 - Create Platforms
-    createPlatform(x=270, y=615, width=200, height=15, color="black");
-    createPlatform(x=600, y=500, width=80, height=15, color="black");
-    createPlatform(x=1000, y=605, width=20, height=100000, color="black");
-    createPlatform(x=1000, y=605, width=200, height=15, color="black");
-    createPlatform(x=100, y=360, width=200, height=15, color="black")
-    // TODO 3 - Create Collectables
-
-    
-
-    // TODO 4 - Create Cannons
-    createCannon("bottom", position=500, delay=600);
-    createCannon("right", position=650, delay=1500);
+ function setup() {
+   if (firstTimeSetup) {
+     halleImage = document.getElementById("player");
+     projectileImage = document.getElementById("projectile");
+     cannonImage = document.getElementById("cannon");
+     $(document).on("keydown", handleKeyDown);
+     $(document).on("keyup", handleKeyUp);
+     firstTimeSetup = false;
+     //start game
+     setInterval(main, 1000 / frameRate);
+   }
 
 
-    
-    
-    //////////////////////////////////
-    // ONLY CHANGE ABOVE THIS POINT //
-    //////////////////////////////////
-  }
+   // Create walls - do not delete or modify this code
+   createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+   createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+   createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+   createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
-  registerSetup(setup);
+
+   //////////////////////////////////
+   // ONLY CHANGE BELOW THIS POINT //
+   //////////////////////////////////
+
+
+   // TODO 1 - Enable the Grid
+   //  toggleGrid();
+
+
+
+   
+
+   // TODO 2 - Create Platforms
+   createPlatform(100, 550, 400, 20, "blue");       
+   createPlatform(600, 450, 200, 20, "green");      
+   createPlatform(900, 350, 150, 20, "purple");    
+   createPlatform(300, 250, 100, 20, "red");        
+   createPlatform(700, 150, 100, 20, "orange");     
+
+
+  // TODO 3 - Create Collectables
+   createCollectable("diamond", 650, 420, 0, 0.5);   
+   createCollectable("steve", 950, 320, 0, 0.7);     
+   createCollectable("grace", 310, 200, 0, 0);      
+  
+
+
+  
+   // TODO 4 - Create Cannons
+   createCannon("left", 400, 2000);     
+   createCannon("top", 600, 1500);    
+   createCannon("right", 250, 1000);
+
+
+
+  
+  
+   //////////////////////////////////
+   // ONLY CHANGE ABOVE THIS POINT //
+   //////////////////////////////////
+ }
+
+
+ registerSetup(setup);
 });
